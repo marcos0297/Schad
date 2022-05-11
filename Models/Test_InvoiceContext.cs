6cb59a15ef157610e74aefafd096e2e4f16cf61c
mmontero@agilisa.com <m0nt3r0.agilisa>
2022-05-11 11:12:16 -0400

    Delete connection string

--- Models/Test_InvoiceContext.cs
+++ Models/Test_InvoiceContext.cs
@@ -28,7 +28,7 @@ namespace TestProgrammingSchad.Models
             if (!optionsBuilder.IsConfigured)
             {
                 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=AGTDEVL1002\\SQLEXPRESS02;Database=Test_Invoice;User Id=sa;Password=agil.12345;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("");
             }
         }
 
